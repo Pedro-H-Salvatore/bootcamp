0b283233fb7e46bec71fd4c462347589aab15078
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-06-20 15:25:00 -0300

    feat: Finalizando o dia 2 exibindo as informações das variáveis no console

---
+++ Dias/Dia-2/App.js
@@ -0,0 +1,13 @@
+const nomeFilme = "O Poderoso Chefão";
+let nota = 9.2;
+const anoLancamento = 1972;
+const genero = "Crime, Drama";
+
+const informacoes = {
+    nomeFilme,
+    nota: 9.3,
+    anoLancamento,
+    genero
+};
+
+console.log(informacoes);
