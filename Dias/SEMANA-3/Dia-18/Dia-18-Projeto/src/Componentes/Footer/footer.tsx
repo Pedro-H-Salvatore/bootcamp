9b65256177db7cf1d54340d20f626046d2f072c2
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-07-03 11:16:05 -0300

    feat: terminando o dia 18 e 19 com a criação do projeto react e cm a criação dos componentes header, footer e main.

---
+++ Dias/SEMANA-3/Dia-18/Dia-18-Projeto/src/Componentes/Footer/footer.tsx
@@ -0,0 +1,13 @@
+import "../../global.css";
+
+const Footer = () => {
+  return (
+    <div className="footer-content">
+      <footer>
+        <p>&copy; 2023 CineExplorer. Todos os direitos reservados.</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
