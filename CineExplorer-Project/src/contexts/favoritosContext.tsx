80c6769700381793d38e1612aadb881efbfc0629
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-07-28 16:00:56 -0300

    feat: Criação do favoritosContext para que seja possivel adicionar os filmes na página favoritos, e isso se tornou possível a partir do useFavorito.

---
+++ CineExplorer-Project/src/contexts/favoritosContext.tsx
@@ -0,0 +1,67 @@
+import { createContext, useContext, useState, type ReactNode } from "react";
+import type { TipoFilme } from "../types/tipoFilme";
+
+
+type TipoFavortioContext = {
+    favoritos: TipoFilme[],
+    adicionarFavorito: (filme: TipoFilme) => void,
+    removerFavoritos: (id: number) => void,
+    estaFavoritado: (id: number) => boolean,
+}
+
+type FavoritoProviderProps = {
+    children: ReactNode
+}
+
+const FavoritoContext = createContext<TipoFavortioContext | undefined>(undefined);
+
+export function FavoritosProvider ({children}: FavoritoProviderProps){
+    const [favoritos, setFavoritos] = useState<TipoFilme[]>([])
+
+   function adicionarFavorito(filme: TipoFilme) {
+    setFavoritos((favoritosAtuais) => {
+      const jaExiste = favoritosAtuais.some(
+        (favorito) => favorito.id === filme.id,
+      );
+
+      if (jaExiste) {
+        return favoritosAtuais;
+      }
+
+      return [...favoritosAtuais, filme];
+    });
+  }
+  
+  function removerFavoritos (id: number){
+    setFavoritos((favoritosAtuais) => favoritosAtuais.filter((filme) => filme.id !== id),)
+  }
+  
+  function estaFavoritado(id: number){
+    return favoritos.some((filme) => filme.id === id)
+  }
+
+  return(
+    <FavoritoContext.Provider 
+    value={{
+        favoritos,
+        adicionarFavorito,
+        removerFavoritos,
+        estaFavoritado
+    }}
+    >
+        {children}
+    </FavoritoContext.Provider>
+  )
+}
+
+export function useFavoritos() {
+  const contexto = useContext(FavoritoContext);
+
+    if(contexto === undefined){
+        throw new Error("usefavoritos precisa estar dentro do FavoritoProvider")
+    }
+    return contexto
+  }
+  
+
+  
--- CineExplorer-Project/src/main.tsx
+++ CineExplorer-Project/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router";
 import './global.css'
 import App from './App.tsx'
+import { FavoritosProvider } from './contexts/favoritosContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
+    <FavoritosProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FavoritosProvider>
   </StrictMode>,
 )
