0e7d4b1baf2ca6b2b69819c49ee18f05704c7a17
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-08-03 20:21:15 -0300

    fix: Tirando os imports inutilizados da pagina favoritos.

--- CineExplorer-Project/src/favoritos.tsx
+++ CineExplorer-Project/src/favoritos.tsx
@@ -3,18 +3,31 @@ import { useFavoritos } from "./contexts/favoritosContext"
 import ImagemHeroFavorito from './assets/imagem-hero-favoritos.jpg'
 import Secao from "./components/secao"
 import Footer from "./components/footer"
+import { useState } from "react"
+import type { TipoFilme } from "./types/tipoFilme"
+import ModalCardFilme from "./components/modalCardFilme"
 
 
 const Favoritos = () => {
     const { favoritos } = useFavoritos()
+    const [filmeClicado, setFilmeClicado] = useState<TipoFilme | null>(null)
+
+    const fecharModal = () => {
+        (
+            setFilmeClicado(null)
+        )
+    }
 
     const filmesFavoritos = favoritos.map((addAosfavs) => (
-        <div key={addAosfavs.id} 
+        <div key={addAosfavs.id}
+            onClick={() => {
+                setFilmeClicado(addAosfavs);
+            }}
             className="cursor-pointer p-3 w-fit ">
             {addAosfavs.poster_path ? (
-                <img src={`https://image.tmdb.org/t/p/w500${addAosfavs.poster_path}`} 
-                alt={`Capa do filme ${addAosfavs.title}`} 
-                className="h-auto w-40 shrink-0 rounded-lg"/>) 
+                <img src={`https://image.tmdb.org/t/p/w500${addAosfavs.poster_path}`}
+                    alt={`Capa do filme ${addAosfavs.title}`}
+                    className="h-auto w-40 shrink-0 rounded-lg" />)
                 : (<p className="text-white">Imagem indisponível</p>)}
         </div>
     ))
@@ -26,10 +39,11 @@ const Favoritos = () => {
                     <h1 className=" text-3xl"><a className=' text-white'>CINE</a><a className='text-red-600'>EXPLORER</a></h1>
 
                     <div className='flex justify-center gap-6'>
-                        <Link className='text-white hover:text-red-600 transition-color duration-300 ' to={"/"}>Home</Link>
-                        <Link className='text-white hover:text-red-600 transition-colors duration-300' to={"/filme"}>Filmes</Link>
+                         <Link className='text-white hover:text-red-600 transition-color duration-300 ' to={"/"}>Home</Link>
+                        <Link className='text-white hover:text-red-600 transition-color duration-300' to={"/filme"}>Filmes</Link>
+                        <Link className='text-white hover:text-red-600 transition-colors duration-300' to={"/buscar"}>Buscar</Link>
                         <Link className='text-red-600' to={"/favoritos"}>Meus Favoritos</Link>
-                        <Link className='text-white hover:text-red-600 transition-colors duration-300' to={"/detalhes"}>Detalhes</Link>
+                        <Link className='text-white hover:text-red-600 transition-colors duration-300' to={"/contato"}>Contato</Link>
                     </div>
                 </nav>
             </header>
@@ -49,15 +63,21 @@ const Favoritos = () => {
                 </section>
                 <section>
                     <div className="min-h-60">
-                       <Secao title="Favoritos">
-                        <div className="flex flex-wrap">
-                            {favoritos.length === 0 ? <p className="text-white px-4 py-4">Nenhum filme favoritado</p> : filmesFavoritos}
-                        </div>
-                       </Secao>
+                        <Secao title="Favoritos">
+                            <div className="flex flex-wrap">
+                                {favoritos.length === 0 ? <p className="text-white px-4 py-4">Nenhum filme favoritado</p> : filmesFavoritos}
+                            </div>
+                        </Secao>
+                        {filmeClicado && (
+                            <ModalCardFilme
+                                filmeSelecionado={filmeClicado}
+                                fecharModal={fecharModal}
+                            />
+                        )}
                     </div>
                 </section>
             </main>
-            <Footer/>
+            <Footer />
         </div>
     )
 }
