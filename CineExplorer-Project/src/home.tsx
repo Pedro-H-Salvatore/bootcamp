f2217125e9b56d2c3a068f075b59b571a2a4aba3
Pedro-H-Salvatore <pedrosalvatore2112@gmail.com>
2026-08-04 08:32:00 -0300

    feat: Dando funcionalidade ao botão explorar catálogo e adicionando novas rotas para as páginas de contato e busca.

--- CineExplorer-Project/src/home.tsx
+++ CineExplorer-Project/src/home.tsx
@@ -5,10 +5,10 @@ import imagemHero from "./assets/Imagem-hero.jpg"
 
 
 const Home = () => {
-   const navigate = useNavigate()
-   function botaoHome(){
-    navigate("/filme")
-   }
+    const navigate = useNavigate()
+    function botaoHome() {
+        navigate("/filme")
+    }
 
     return (
         <div className="bg-black font-bold min-h-screen">
@@ -17,13 +17,14 @@ const Home = () => {
                     <h1 className=" text-3xl "><a className=' text-white'>CINE</a><a className='text-red-600'>EXPLORER</a></h1>
                     <div className='flex items-start justify-center gap-6'>
                         <Link className='text-red-600' to={"/"}>Home</Link>
-                        <Link className='text-white hover:text-red-600 transition-colors duration-300' to={"/filme"}>Filmes</Link>
+                        <Link className='text-white hover:text-red-600 transition-color duration-300' to={"/filme"}>Filmes</Link>
+                        <Link className='text-white hover:text-red-600 transition-colors duration-300' to={"/buscar"}>Buscar</Link>
                         <Link className='text-white hover:text-red-600 transition-colors duration-300' to={"/favoritos"}>Meus Favoritos</Link>
-                        <Link className='text-white hover:text-red-600 transition-colors duration-300' to={"/detalhes"}>Detalhes</Link>
+                        <Link className='text-white hover:text-red-600 transition-colors duration-300' to={"/contato"}>Contato</Link>
                     </div>
                 </nav>
             </header>
-        
+
             <main>
                 <section className='relative w-full h-150'>
                     <img src={imagemHero} alt="" className="w-full h-full object-cover" />
@@ -32,7 +33,7 @@ const Home = () => {
                         <h2 className="text-white text-5xl font-bold">
                             TODA HISTÓRIA COMEÇA COM UM PLAY
                         </h2>
-                        <button onClick={botaoHome} className="bg-red-600 text-white w-fit px-6 py-3 mt-6 rounded-lg hover:bg-red-700 hover:text-gray-200 transition-colors duration-300">
+                        <button onClick={botaoHome} className="cursor-pointer bg-red-600 text-white w-fit px-6 py-3 mt-6 rounded-lg hover:bg-red-700 hover:text-gray-200 transition-colors duration-300">
                             EXPLORAR CATÁLOGO
                         </button>
 
